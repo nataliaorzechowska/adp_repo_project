6d9052d34cd90ad4bc6181c549723a47776ceee8
Natalia Orzechowska <s444336@labs.wmi.amu.edu.pl>
2020-06-14 20:55:33 +0200

    wykresy dla wojewodztw

--- R/r_test_script.R
+++ R/r_test_script.R
@@ -5,8 +5,6 @@ library(readxl)
 library(sf)
 library(corrplot)
 
-install.packages("Hmisc")
-
 library("Hmisc")
 
 powiaty <- st_read( dsn = 'Data/jednostki_administracyjne/PRG_jednostki_administracyjne_v40_SZPRG/Powiaty.shp' )
@@ -18,6 +16,7 @@ x_absolwenci <- readxl::read_xlsx('Data/absolwenci.xlsx', sheet = 2)
 
 View(y_rozwody)
 View(x_pracujacy_sekcje)
+View(x_wynagrodzenie)
 
 powiaty$JPT_KOD_JE <- paste0(powiaty$JPT_KOD_JE,'000')
 
@@ -60,6 +59,8 @@ final_df <- powiaty %>% left_join(fourth_df, by = c ( 'JPT_KOD_JE' = 'Kod'))
 
 final_df %>% summary()
 
+View(final_df)
+
 final_df <- rename(final_df,przecietne_miesieczne_wynagrodzenie = `przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y`)
 
 #długo się ładuje 
@@ -67,11 +68,16 @@ final_df <- rename(final_df,przecietne_miesieczne_wynagrodzenie = `przeciętne m
 
 dane_bez_sf <- fourth_df
 
+View(dane_bez_sf)
+dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y` <- NULL
+
 summary(dane_bez_sf)
+
+View(fourth_df)
 
 # z tekstu na liczby
 
-dane_bez_sf$ilosc_rozwodow = as.numeric(as.character(dane_bez_sf$ilosc_rozwodow ))
+dane_bez_sf$rozwody = as.numeric(as.character(dane_bez_sf$rozwody ))
 dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).x` = 
   as.numeric(as.character(dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).x` ))
 dane_bez_sf$`rolnictwo, leśnictwo, łowiectwo i rybactwo` = as.numeric(as.character(dane_bez_sf$`rolnictwo, leśnictwo, łowiectwo i rybactwo`))
@@ -81,8 +87,6 @@ dane_bez_sf$`handel; naprawa pojazdów samochodowych; transport i gospodarka mag
 dane_bez_sf$`działalność finansowa i ubezpieczeniowa; obsługa rynku nieruchomości` = 
   as.numeric(as.character(dane_bez_sf$`działalność finansowa i ubezpieczeniowa; obsługa rynku nieruchomości`)) 
 dane_bez_sf$`pozostałe usługi` = as.numeric(as.character(dane_bez_sf$`pozostałe usługi`)) 
-dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y` = 
-  as.numeric(as.character(dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y`)) 
 dane_bez_sf$`absolwenci ogółem` = as.numeric(as.character(dane_bez_sf$`absolwenci ogółem`))
 
 
@@ -91,8 +95,12 @@ dane_bez_sf$`absolwenci ogółem` = as.numeric(as.character(dane_bez_sf$`absolwe
 #mediany <- lapply(dane_bez_sf, 0 , na.rm = TRUE)
 dane_bez_sf[is.na(dane_bez_sf)] <-0
 
+dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y` <- NULL
+
+View(dane_bez_sf)
+
 dane_bez_sf <- dane_bez_sf %>%
-  rename(rozw = ilość_rozwodów,
+  rename(rozwody = `ogółem`,
          wynag =`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).x`,
          rolnic = `rolnictwo, leśnictwo, łowiectwo i rybactwo`,
          przem = `przemysł i budownictwo`,
@@ -101,14 +109,14 @@ dane_bez_sf <- dane_bez_sf %>%
          poz_usl = `pozostałe usługi`,
          absol =  `absolwenci ogółem`)
 
-dane_bez_sf$`przeciętne miesięczne wynagrodzenia brutto w relacji do średniej krajowej (Polska=100).y` <- NULL
-
 View(dane_bez_sf)
 
 # macierz korelacji 
 macierz_korelacji <-cor(dane_bez_sf[sapply(dane_bez_sf,is.numeric)])
 
-corrplot(macierz_korelacji,method="color")
+
+corrplot(macierz_korelacji, method = "circle" )
+dev.off
 
 # duza dodatnia korelacja miedzy liczba rozwodow a iloscia absolwentów
 # mała dodatnia korelacja miedzy liczbą rozwodów a wynagrodzeniem 
@@ -132,27 +140,54 @@ ggplot(dane_z_sf,aes(fill = wynag )) + geom_sf()+
 ggplot(dane_z_sf,aes(fill = absol )) + geom_sf()+
   scale_fill_gradient( low = 'white' ,  high = 'dark blue')
 
+
+# ogarniamy rozwody w wojewodztwach
 wojewodztwa <- dane_bez_sf %>% select(Nazwa.x,Kod) %>% filter(Nazwa.x == toupper(Nazwa.x))
 wojewodztwa <- wojewodztwa[-1,]
 View(wojewodztwa)
+wojewodztwa$woj <- (substr(wojewodztwa$Kod,1,2))
 
-dane_bez_sf$woj <- 'text'
+  dane_bez_sf$woj <- 'text'
+  dane_bez_sf$woj <- (substr(dane_bez_sf$Kod,1,2))
 
+ 
 
-# bedzie dodanie kolumny z wojewodztwem zeby zrobic dla kazdego wojewodztwa osobne histogramy 
-for ( i in  1:nrow(wojewodztwa)){
-  nazwa_woj <- wojewodztwa$Nazwa.x[i]
-  kod_dwie_pierwsze <- substr(wojewodztwa$Kod[i],1,2)
-  for (j in 1:nrow(dane_bez_sf)){
-    if ( substr(dane_bez_sf$Kod[j],1,2) == kod_dwie_pierwsze){
-      dane_bez_sf$woj <- nazwa_woj
-    }
-  }
-}
-
+dane_bez_sf<-merge(dane_bez_sf,wojewodztwa,by = "woj",all.x = TRUE)
 View(dane_bez_sf)
 
 
+dane_bez_sf <- dane_bez_sf %>%
+  rename(wojewodztwo = `Nazwa.x.y`,
+         )
+dane_bez_sf$Kod.y <- NULL
+
+
+
+ggplot(dane_bez_sf,aes(wojewodztwo))+geom_bar() + ylab("Rozwody")+
+  geom_line(wynag)+
+  theme(axis.text.x=element_text(angle =- 90, vjust = 0.5))
+
+
+
+# juz moze byc :) dodamy jeszcze podział na grupy zawodowe 
+
+# srednie zarobki w wojewodztwie
+
+srednie_zarobki_wojewodztwa <- dane_bez_sf %>% group_by(wojewodztwo) %>%
+  summarise(srednie_zarobki = mean(wynag))
+
+View(srednie_zarobki_wojewodztwa)
+
+ggplot(dane_bez_sf,aes(wojewodztwo))+
+  geom_bar() + 
+  ylab("Rozwody")+
+  theme(axis.text.x=element_text(angle =- 90, vjust = 0.5))
+
+ggplot(srednie_zarobki_wojewodztwa,aes(wojewodztwo,srednie_zarobki,group =1)) +
+  geom_point(aes(y = srednie_zarobki_wojewodztwa$srednie_zarobki))+
+  geom_line()+
+  ylab("Średnie zarobki")+
+  theme(axis.text.x=element_text(angle =- 90, vjust = 0.5))
 
 
 
